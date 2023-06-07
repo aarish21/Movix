8a17159cc56f9ae5afa4292b8485ed72b67ff21b
aarish21 <aarish.jerry@gmail.com>
2023-06-07 22:36:54 +0530

    updated file name

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ import {
     FaLinkedin,
 } from "react-icons/fa";
 
-import ContentWrapper from "../ContentWrapper/ContentWrapper";
+import ContentWrapper from "../contentWrapper/ContentWrapper";
 
 import "./style.scss";
 
